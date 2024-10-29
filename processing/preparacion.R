bbad759ea7dea3f79eb3c6a3c03d040dddff037e
mafenunez <146782034+mafenunez@users.noreply.github.com>
2024-10-29 15:18:21 -0300

    analisis

--- processing/preparacion.R
+++ processing/preparacion.R
@@ -17,21 +17,38 @@ load("input/data/proc/ola1.RData")
 
 # 3. procesamiento de variables -----------------------------------------------
 
+# rendimiento ----
+
+proc_datos <- proc_datos %>% rename(rendimiento = nota_obtenida)
+
 # evaluacion de justicia ----
 
-proc_datos <- proc_datos %>% mutate(justicia_nota = log(nota_obtenida/nota_pref))
+# nota preferida 
+proc_datos <- proc_datos %>%
+  mutate(nota_pref = case_when(
+    is.na(nota_pref) & notas_merit == 2 ~ rendimiento ,  # Imputar el valor de 'notas_merit' (2) cuando 'nota_pref' es NA
+    TRUE ~ nota_pref  # Mantener los valores existentes de 'nota_pref' cuando no son NA
+  ))
+
+# justicia 
+proc_datos <- proc_datos %>% mutate(justicia_nota = log(rendimiento/nota_pref))
+                                    
+                              
 
 summary(proc_datos$justicia_nota)
+
+
+## justicia categorica
 
 # prueba con pregunta que añade tiempo 
 proc_datos <- proc_datos %>%
   mutate(
-    justicia_nota_tiempo = dplyr::case_when(
+    justicia_nota_tiempo_ch = dplyr::case_when(
       justicia_nota < 0  |notas_esfuerzo == 1 ~ 1,
       justicia_nota == 0 | notas_esfuerzo == 2 ~ 2, 
       justicia_nota > 0  | notas_esfuerzo == 3 ~ 3)) 
 
-proc_datos$justicia_nota_tiempo <- factor(proc_datos$justicia_nota_tiempo, 
+proc_datos$justicia_nota_tiempo_ch <- factor(proc_datos$justicia_nota_tiempo_ch, 
                                           levels=c(1,2,3),
                                           labels=c("injusticia sub-recompensa",
                                                    "justicia perfecta",
@@ -40,20 +57,19 @@ proc_datos$justicia_nota_tiempo <- factor(proc_datos$justicia_nota_tiempo,
 
 proc_datos <- proc_datos %>%
   mutate(
-    justicia_nota = dplyr::case_when(
+    justicia_nota_ch = dplyr::case_when(
       justicia_nota < 0  ~ 1,
-      justicia_nota == 0 | notas_merit == 2 ~ 2, 
+      justicia_nota == 0 ~ 2, 
       justicia_nota > 0  ~ 3
     )
   ) 
 
-proc_datos$justicia_nota <- factor(proc_datos$justicia_nota, 
+proc_datos$justicia_nota_ch <- factor(proc_datos$justicia_nota_ch, 
                                           levels=c(1,2,3),
                                           labels=c("injusticia sub-recompensa",
                                                    "justicia perfecta",
                                                    "injusticia sobre-recompensa"))
-frq(proc_datos$justicia_nota)  
-
+frq(proc_datos$justicia_nota_ch)  
 
 
 # 4. base procesada -----------------------------------------------------------
